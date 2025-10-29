cc432b3fc99cb39f84660b06cf51fd549f345dcd
Sofia Barrios <sofiabarrios@Sofias-MacBook-Pro-9.local>
2025-10-28 20:09:28 -0500

    fix: remove redundant null-check

--- src/main/java/org/example/Review1.java
+++ src/main/java/org/example/Review1.java
@@ -2,9 +2,10 @@ package org.example;
 
 public class Review1 {
     public void method1(){
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < 1000; i++) {
-            result = result + i;
+            result.append(i);
         }
+        System.out.println(result);
     }
 }
